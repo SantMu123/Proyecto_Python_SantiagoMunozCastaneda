63ff17ba3528977e699ad56cd9830e0c7b706298
SantMu123 <smunozcastaneda14@gmail.com>
2024-02-15 18:53:34 -0500

    Avance_Proyecto_15_02_2024 #1

---
+++ ModuloEstudiante.py
@@ -0,0 +1,67 @@
+import menu
+import json
+
+def cargar_lista_estudiantes():
+    try:
+        with open('Campers.json', 'r') as file:
+            lista_estudiantes = json.load(file)
+    except FileNotFoundError:
+        print("Error")
+    return lista_estudiantes
+
+def guardar_lista_estudiantes(lista_estudiantes):
+    with open('Campers.json', 'w') as file:
+        json.dump(lista_estudiantes, file)
+
+def agregar_estudiante(lista_estudiantes, info):
+    lista_estudiantes.append(info)
+
+comando = "A"
+while comando != "E":
+    comando = menu.MenuCamper()
+    if comando == "A":
+        
+        try: 
+            nombre = input("Digita tu nombre: ")
+            apellido = input("Digita tu apellido")
+            direccion = input("Digita tu direccion: ")
+            asistente = input("Digita tu attendant: ")
+            numeroCel = input("Digita tu numero CEL: ")
+            numeroTel = input("Digita tu numero TEl: ")
+            
+            info = {
+            "name": nombre,
+            "Second name": apellido,
+            "Adress": direccion,
+            "attendant": asistente,
+            "Number Contact #1": numeroCel,
+            "Number Contact #2": numeroTel,
+            "Status": "Inscrito",
+            "Risk": "None",
+            "Teoric Note": "None",
+            "Practical Note": "None",
+            "Average Note": "None"
+            }
+            campers = cargar_lista_estudiantes()
+            agregar_estudiante(campers, info)
+            guardar_lista_estudiantes(campers)
+            #1314   
+        except Exception as e:
+            print("Registro Fallido, Vuleve a intentarlo!!!")
+            print(e)
+            comando = menu.MenuCamper()
+    if comando == "B":
+        print("Entraste a la opción de Ver Notas!!!")
+        nombre = input("Ingresa tu nombre: ")
+        apellido = input("Ingresa tu apellido")
+        """
+        if nombre in listaCampers['name'] and apellido in listaCampers['Second name']:
+            print("Tus notas son: ")
+        else:
+            print("No se ha encontrado Camper en la base de datos")
+            
+        """    
+        
+        
+        
+        
